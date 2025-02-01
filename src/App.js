5234ec2e5914aa158399103945e05f24797f566a
Someswarklizos2001 <someswar@klizos.com>
2025-02-01 15:57:38 +0530

    cherrypick

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
           }}
           onClick={(e) => dispatch(increment())}
         >
-          increment main
+          increment cherrypick
         </Button>
         <Button
           variant="contained"
@@ -56,7 +56,7 @@ function App() {
           }}
           onClick={(e) => dispatch(decrement())}
         >
-          decrement main
+          decrement cherrypick
         </Button>
       </Box>
 
