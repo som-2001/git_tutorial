e82e8647e005bcfb0e1b0c6ae9c394be14859f9f
Someswarklizos2001 <someswar@klizos.com>
2025-02-01 21:02:32 +0530

    update from master branch

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,5 +1,8 @@
 export const Cart=()=>{
     return(
-        <h1>i have updated the cart function from main branch.</h1>
+        <>
+        <h2>i have updated the cart function from main branch.</h2>
+        <h1>Master branch</h1>
+        </>
     )
 } 
